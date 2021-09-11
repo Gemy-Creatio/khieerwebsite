76ac445ebe53a113fcdf3150143c888efd634920
Gemy-star <m.yassen.93@gmail.com>
2021-09-11 02:07:15 +0200

    final green

--- greenCircle/urls.py
+++ greenCircle/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('greenCircle', views.greenCircle, name='green-circle')
+    path('greenCircle', views.greenCircle, name='green-circle'),
+    path('greenCourse/<int:pk>', views.greenCircleCourses, name='green-courses'),
+    path('greenTrainer/<int:pk>', views.green_trainer_details, name='green-trainer')
+
 ]
--- greenCircle/views.py
+++ greenCircle/views.py
@@ -5,3 +5,13 @@ from greenCircle.models import Category, Trainer, CourseRequest, Course
 def greenCircle(request):
     context = {"cats": Category.objects.all()}
     return render(request, 'greenCircle/greenCircle.html', context)
+
+
+def greenCircleCourses(request, pk):
+    context = {"courses": Course.objects.filter(category_id=pk)}
+    return render(request, 'greenCircle/greencourses.html', context=context)
+
+
+def green_trainer_details(request, pk):
+    context = {"trainer": Trainer.objects.get(course=pk)}
+    return render(request, 'greenCircle/greentrainer.html', context=context)
