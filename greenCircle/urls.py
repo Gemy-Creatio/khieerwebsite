40596e620f719bfe2c63c55921fe4ff699a5fb17
Gemy-star <m.yassen.93@gmail.com>
2021-09-11 10:26:34 +0200

    1289

--- greenCircle/urls.py
+++ greenCircle/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('greenCircle', views.greenCircle, name='green-circle'),
     path('greenCourse/<int:pk>', views.greenCircleCourses, name='green-courses'),
-    path('greenTrainer/<int:pk>', views.green_trainer_details, name='green-trainer')
+    path('greenTrainer/<int:pk>', views.green_trainer_details, name='green-trainer'),
+    path('greenCourse/add/<int:pk>', views.course_request, name='green-add-user')
 
 ]
--- greenCircle/views.py
+++ greenCircle/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from greenCircle.models import Category, Trainer, CourseRequest, Course
 
 
@@ -15,3 +15,17 @@ def greenCircleCourses(request, pk):
 def green_trainer_details(request, pk):
     context = {"trainer": Trainer.objects.get(course=pk)}
     return render(request, 'greenCircle/greentrainer.html', context=context)
+
+
+def course_request(request, pk):
+    course = Course.objects.get(pk=pk)
+    context = {"course": course}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        course_obj = CourseRequest(name=name, phone=phone, email=email, course=course)
+        course_obj.save()
+        if course_obj.pk:
+            return redirect('green-circle', course.category.id)
+    return render(request, 'greenCircle/greencourse_request.html', context)
